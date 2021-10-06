4aeee358c3b02874d8701753e9be6b428c816ede
Jakson Casas <jaksoncasas@gmail.com>
2021-10-05 22:36:00 -0500

    Routing configuracion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { EmployeeComponent } from './employee/employee.component';
+import { DeparmentComponent } from './deparment/deparment.component';
+
+const routes: Routes = [
+  {path:'employee',component:EmployeeComponent},
+  {path:'department',component:DeparmentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
