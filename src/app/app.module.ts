08e6c16350498aff4ee44161594ae1af6d193baf
Jakson Casas <jaksoncasas@gmail.com>
2021-10-05 22:07:13 -0500

    Employee component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { DeparmentComponent } from './deparment/deparment.component';
 import { ShowDepComponent } from './deparment/show-dep/show-dep.component';
 import { AddEditDepComponent } from './deparment/add-edit-dep/add-edit-dep.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ShowEmpComponent } from './employee/show-emp/show-emp.component';
+import { AddEditEmpComponent } from './employee/add-edit-emp/add-edit-emp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DeparmentComponent,
     ShowDepComponent,
-    AddEditDepComponent
+    AddEditDepComponent,
+    EmployeeComponent,
+    ShowEmpComponent,
+    AddEditEmpComponent
   ],
   imports: [
     BrowserModule,
